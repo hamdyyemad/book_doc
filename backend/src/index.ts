58752f9d03dd44fcb7d4461f2675ee33148e5614
hamdyyemadd <hamdyyemad@aol.com>
2024-09-01 19:05:13 +0300

    backend starter files
    backend starter files

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World with TypeScript!");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI || "")
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((error) => console.log(error));
